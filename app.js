b14eba4c444c398defa9af77ec64148b7576ff54
Dionata Mesquita <dionata.bm@gmail.com>
2021-05-12 18:10:12 -0300

    Api ticket conected in db

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require ( 'express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 
 const rotaTicket = require('./routes/ticket');
 const rotaOrder = require('./routes/order');
 
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+      'Access-Control-Allow-Header',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET');
+      return res.status(200).send({});
+  }
+  next();
+});
+
+
 app.use('/ticket', rotaTicket);
 app.use('/order', rotaOrder);
 
--- routes/ticket.js
+++ routes/ticket.js
@@ -1,22 +1,54 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const oracledb = require('oracledb');
+var password = 'senha';
 
-router.get('/', (req, res, next) => {
-  res.status(200).send({
-    mensagem: 'Get todos os Boletos'
-  });
-})
-router.get('/:idTicket', (req, res, next) => {
-  const id = req.params.idTicket
-  if (id ==='especial') {
-    res.status(200).send({
-      mensagem: 'Get Boletos pelo id!',
-      id: id
+
+async function selectTicketsById(req, res, id) {
+  try {
+    connection = await oracledb.getConnection({
+      user: 'user',
+      password: password,
+      connectString: "localhost:1521/orcl"
     });
-  } else {
-    res.status(200).send({
-      mensagem: 'Voce passou o id',
-  });
+    let query = 'SELECT cod, cliente FROM client where cod=:id';
+    // run query to get employee with employee_id
+    result = await connection.execute(query, [id]);
+
+  } catch (err) {
+    //send error message
+    return res.send(err.message);
+  } finally {
+    if (connection) {
+      try {
+        // Always close connections
+        await connection.close(); 
+      } catch (err) {
+        return console.error(err.message);
+      }
+    }
+    if (result.rows.length == 0) {
+      //query return zero employees
+      return res.send('query send no rows');
+    } else {
+      //send all employees
+      return res.send(result.rows);
+    }
+  }
 }
+
+//get /employee?id=<id employee>
+router.get('/cpf', function (req, res) {
+  //get query param ?id
+  let id = req.query.id;
+  // id param if it is number
+  if (isNaN(id)) {
+    res.send('Query param id is not number')
+    return
+  }
+  selectTicketsById(req, res, id);
 })
+
+///app.listen(port, () => console.log("nodeOracleRestApi app listening on port %s!", port))
+
 module.exports = router;
